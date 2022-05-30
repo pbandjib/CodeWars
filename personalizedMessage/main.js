3ff3b72557788b5218d1cac878d7b2d4afa8e125
Jay <pbandjib@gmail.com>
2022-05-29 22:58:49 -0400

    codewars

---
+++ personalizedMessage/main.js
@@ -0,0 +1,11 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+function greet (name, owner) {
+    if(name === owner){
+      return 'Hello boss'
+    }else{
+      return 'Hello guest'
+    }
+  }
