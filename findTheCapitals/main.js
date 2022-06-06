e7c0c21f56bd05fb08ffdebc6d09f8dc43160c21
Jay <pbandjib@gmail.com>
2022-06-05 23:30:00 -0400

    codewars

---
+++ findTheCapitals/main.js
@@ -0,0 +1,25 @@
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+
+var capitals = function (word) {
+    var caps = [];
+    for(var i = 0; i < word.length; i++) {
+      if(word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+  };
+
+
+
+
+
+//sample test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(capitals('CodEWaRs'), [0,3,4,6] );
+  });
+});
