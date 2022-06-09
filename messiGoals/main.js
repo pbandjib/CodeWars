703c19125fac9666ad9f5b7e2cac096e853b9088
Jay <pbandjib@gmail.com>
2022-06-08 21:53:30 -0400

    codewars

---
+++ messiGoals/main.js
@@ -0,0 +1,23 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+//sample test 
+
+describe("total goals", function(){
+    it ("should equal the total", function () {
+      Test.assertEquals(totalGoals, 58)
+    })
+})
